3aabb4d8dfc4bfc39df7b7e3aea195a0aa0912d6
stefandmn <damian.stefan@gmail.com>
2019-01-04 14:57:23 +0200

    Release mcrep/skin.clue-1.4.6

--- buildpart.sh
+++ buildpart.sh
@@ -10,27 +10,27 @@
 ###############################################################################
 
 # Run TexturePacker for default theme
-if [[ "${PKR}" = -*d* ]]; then
+if [[ "${PKR}" = -*d* ]] || [[ "${PKR}" = -*a* ]]; then
 	${BASEPATH}/.tools/TexturePacker -input "${RESDIR}/themes/Defaults" -output "${SRCDIR}/media/Textures.xbt"
 fi
 
 # Run TexturePacker for Blue theme
-if [[ "${PKR}" = -*b* ]]; then
+if [[ "${PKR}" = -*b* ]] || [[ "${PKR}" = -*a* ]]; then
 	${BASEPATH}/.tools/TexturePacker -input "${RESDIR}/themes/Blue" -output "${SRCDIR}/media/Blue.xbt"
 fi
 
 # Run TexturePacker for Crimson theme
-if [[ "${PKR}" = -*c* ]]; then
+if [[ "${PKR}" = -*c* ]] || [[ "${PKR}" = -*a* ]]; then
 	${BASEPATH}/.tools/TexturePacker -input "${RESDIR}/themes/Crimson" -output "${SRCDIR}/media/Crimson.xbt"
 fi
 
 # Run TexturePacker for Lime theme
-if [[ "${PKR}" = -*l* ]]; then
+if [[ "${PKR}" = -*l* ]] || [[ "${PKR}" = -*a* ]]; then
 	${BASEPATH}/.tools/TexturePacker -input "${RESDIR}/themes/Lime" -output "${SRCDIR}/media/Lime.xbt"
 fi
 
 # Run TexturePacker for Magenta theme
-if [[ "${PKR}" = -*m* ]]; then
+if [[ "${PKR}" = -*m* ]] || [[ "${PKR}" = -*a* ]]; then
 	${BASEPATH}/.tools/TexturePacker -input "${RESDIR}/themes/Magenta" -output "${SRCDIR}/media/Magenta.xbt"
 fi
 
